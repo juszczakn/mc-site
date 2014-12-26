80c019ba804efdea548edd7e9cee5efe4d5c26b9
Nick Juszczak <juszczakn@gmail.com>
2014-12-26 16:54:21 -0600

    add index.html, main.css, projects.html for /

--- src/mc_site/core/handler.clj
+++ src/mc_site/core/handler.clj
@@ -4,7 +4,7 @@
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] (clojure.java.io/resource "public/index.html"))
   (route/not-found "Not Found"))
 
 (def app
